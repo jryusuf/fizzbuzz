1c4846140ff0e29861cb4419618ca77ed8eaaa4c
jryusuf <yusufyuksel@aol.com>
2025-04-17 12:16:40 +0100

    refactor fizzbuzz

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -2,13 +2,20 @@ import logging
 from typing import Callable, Iterator
 
 def get_fizz_buzz_string(number: int) -> str:
-    if number % 3 ==0 and number % 5 == 0:
-        return str(number) +" fizzbuzz"
-    if number % 3 == 0:
-        return str(number) +" fizz"
-    if number % 5 == 0:
-        return str(number) +" buzz"
-    return str(number)
+    num_str = str(number)
+    suffix = ""
+
+    divisible_by_3 = (number % 3 == 0)
+    divisible_by_5 = (number % 5 == 0)
+
+    if divisible_by_3 and divisible_by_5:
+        suffix = " fizzbuzz"
+    elif divisible_by_3:
+        suffix = " fizz"
+    elif divisible_by_5:
+        suffix = " buzz"
+
+    return num_str + suffix
 
 def generate_fizz_buzz_for_range(
     start: int,
